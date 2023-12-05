1e4d9db702ab39aaf087cc2d577c82a05a270ff9
fomes <fgomesdeluna@gmail.com>
2023-12-05 09:53:36 -0300

    Fix ts warn

--- src/redux/cart/cartSelectors.ts
+++ src/redux/cart/cartSelectors.ts
@@ -2,14 +2,14 @@ import { Product } from "@/types/product";
 
 export const selectProductsCount = (rootReducer: any) => {
   return rootReducer.cartSlice.products.reduce(
-    (acc: number, curr: Product) => acc + curr.quantity,
+    (acc: number, curr: Product) => acc + curr.quantity!,
     0
   );
 };
 
 export const selectTotalPrice = (rootReducer: any) => {
   return rootReducer.cartSlice.products.reduce(
-    (acc: number, curr: Product) => acc + curr.price * curr.quantity,
+    (acc: number, curr: Product) => acc + curr.price * curr.quantity!,
     0
   );
 };
